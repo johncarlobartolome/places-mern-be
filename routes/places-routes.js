b4a5e17e911fb6e4e23f41b1121759be54d319f9
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-05-02 14:16:39 +0800

    Add delete logic for places

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -12,6 +12,6 @@ router.post("/", placesControllers.createPlace);
 
 router.patch("/:pid", placesControllers.updatePlace);
 
-// router.delete("/:pid");
+router.delete("/:pid", placesControllers.deletePlace);
 
 module.exports = router;
