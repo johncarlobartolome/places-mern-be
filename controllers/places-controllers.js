63710943b1855abeb7ce294a533e01497dbe94b9
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-05-05 02:52:13 +0800

    Adjust get places by id controller to populate

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -32,9 +32,9 @@ const getPlaceById = async (req, res, next) => {
 
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate("places");
   } catch (err) {
     console.log(err);
     const error = new HttpError(
@@ -44,13 +44,15 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError("Could not find places for the provided user id.", 404)
     );
   }
   res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.places.map((place) =>
+      place.toObject({ getters: true })
+    ),
   });
 };
 
