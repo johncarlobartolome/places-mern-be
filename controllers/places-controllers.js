508408629cd86fa388a4761031d508bb265684f3
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-05-06 18:19:57 +0800

    Refactor createPlace function to use userId from request data and remove unnecessary creator parameter

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -64,7 +64,7 @@ const createPlace = async (req, res, next) => {
       new HttpError("Invalid inputs passed, please check your data.", 422)
     );
   }
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
 
   let coordinates;
   try {
@@ -79,12 +79,12 @@ const createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image: req.file.path,
-    creator,
+    creator: req.userData.userId,
   });
 
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(createdPlace.creator);
   } catch (err) {
     const error = new HttpError(
       "Creating place failed, please try again.",
